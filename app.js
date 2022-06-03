4358b78d48084b3ce8dfbf0bb9540fdd6dccf1e5
Surajit Dhar <surajitgokarna@gmail.com>
2022-06-03 12:23:59 +0530

    finally

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const posts = require('./posts');
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+    max : 10,
+    windowMs: 60000
+})
+
+app.get('/posts', limiter, (req, res) => {
+    res.send({
+        status: "Success",
+        posts : posts
+    })
+})
+ app.listen(8000, ()=> console.log("Listening on port 8000"));
